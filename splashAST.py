4906eb1852734810358f2715a0366d49bd3128e2
Mangg0 <t.p.algarvio@gmail.com>
2023-04-13 00:09:53 +0100

    AST started

--- splashAST.py
+++ splashAST.py
@@ -1,109 +1,79 @@
-import sys
-from typing import List
 from dataclasses import dataclass
+from typing import List
 
-from lark import Lark, ast_utils, Transformer, v_args
+from lark  import Lark, ast_utils, Transformer, v_args
 from lark.tree import Meta
 
-this_module = sys.modules[__name__]
+
+from dataclasses import dataclass
+from typing import List
 
 
-#
-#   Define AST
-#
-class _Ast(ast_utils.Ast):
-    # This will be skipped by create_transformer(), because it starts with an underscore
+
+class _ASTNode(ast_utils.Ast):
     pass
 
-class _Statement(_Ast):
-    # This will be skipped by create_transformer(), because it starts with an underscore
+class _Statement(_ASTNode):
     pass
 
 @dataclass
-class Value(_Ast, ast_utils.WithMeta):
-    "Uses WithMeta to include line-number metadata in the meta attribute"
-    meta: Meta
-    value: object
-
-@dataclass
-class Name(_Ast):
-    name: str
-
-@dataclass
-class CodeBlock(_Ast, ast_utils.AsList):
-    # Corresponds to code_block in the grammar
+class Program:
     statements: List[_Statement]
 
 @dataclass
-class If(_Statement):
-    cond: Value
-    then: CodeBlock
-
-@dataclass
-class SetVar(_Statement):
-    # Corresponds to set_var in the grammar
+class Declaration:
     name: str
-    value: Value
+    type: str
+    refinement: 'Condition' = None
 
 @dataclass
-class Print(_Statement):
-    value: Value
+class Definition:
+    name: str
+    parameters: List['Binder']
+    body: 'Block'
 
+@dataclass
+class Binder:
+    name: str
+    type: str
+    refinement: 'Condition' = None
 
-class ToAst(Transformer):
-    # Define extra transformation functions, for rules that don't correspond to an AST class.
+@dataclass
+class Block:
+    statements: List[_Statement]
+
+@dataclass
+class IfThenElse:
+    condition: 'Condition'
+    if_block: 'Block'
+    else_block: 'Block' = None
+
+@dataclass
+class While:
+    condition: 'Condition'
+    block: 'Block'
+
+@dataclass
+class Return:
+    expression: 'Expression' = None
+
+@dataclass
+class Add:
+    left: 'Expression'
+    right: 'Expression'
+
+class toAST(Transformer):
+    
 
     def STRING(self, s):
-        # Remove quotation marks
         return s[1:-1]
+    
+    def INT(self, i):
+        return int(i)
+        
+    def FLOAT(self, i):
 
-    def DEC_NUMBER(self, n):
-        return int(n)
 
     @v_args(inline=True)
     def start(self, x):
         return x
-
-#
-#   Define Parser
-#
-
-parser = Lark("""
-    start: code_block
-
-    code_block: statement+
-
-    ?statement: if | set_var | print
-
-    if: "if" value "{" code_block "}"
-    set_var: NAME "=" value ";"
-    print: "print" value ";"
-
-    value: name | STRING | DEC_NUMBER
-    name: NAME
-
-    %import python (NAME, STRING, DEC_NUMBER)
-    %import common.WS
-    %ignore WS
-    """,
-    parser="lalr",
-)
-
-transformer = ast_utils.create_transformer(this_module, ToAst())
-
-def parse(text):
-    tree = parser.parse(text)
-    return transformer.transform(tree)
-
-#
-#   Test
-#
-
-if __name__ == '__main__':
-    print(parse("""
-        a = 1;
-        if a {
-            print "a is 1";
-            a = 2;
-        }
-    """))
--- splash_parser.py
+++ splash_parser.py
@@ -1,7 +1,12 @@
 import sys
 import os
 import argparse
+from dataclasses import dataclass
+from typing import List
+
 from lark import Lark, Transformer, v_args
+from lark import ast_utils
+
 
 with open("grammars/sPLash.lark") as _grammar:
     grammar = _grammar.read()
@@ -18,6 +23,79 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument("--tree", help="print AST", action="store_true")
 argparser.add_argument("file", help="file to parse")
 argsp = argparser.parse_args()
+
+
+@dataclass
+class _ASTNode(ast_utils.Ast):
+    pass
+
+
+class _Statement(_ASTNode):
+    pass
+
+
+
+@dataclass
+class _Program(_ASTNode):
+    statements: List[_Statement]
+
+
+
+
+@dataclass
+class Declaration(_Statement):
+        type_ : str
+        name : str 
+        refinements : List[Refinement]
+
+@dataclass
+class Refinement(_ASTNode):
+    refinement: Condition
+
+
+@dataclass
+class Definition(_Statement):
+    def __init__(self, name, args, block):
+        self.name: name
+        self.args = args or []
+        self.block = block
+
+class BinaryOp:
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+class UnaryOp:
+    def __init__(self, op, expr):
+        self.op = op
+        self.expr = expr
+
+class Literal:
+    def __init__(self, value, type_):
+        self.value = value
+        self.type_ = type_
+
+class IfThenElse:
+    def __init__(self, condition, if_block, else_block=None):
+        self.condition = condition
+        self.if_block = if_block
+        self.else_block = else_block
+
+@dataclass
+class While(_Statement):
+    condition: Condition
+    block: Block
+
+class Return:
+    pass
+
+
+
+class Block(_ASTNode):
+    statements: List[_Statement]
+
+
 
 def test(to_parse: str):
 
